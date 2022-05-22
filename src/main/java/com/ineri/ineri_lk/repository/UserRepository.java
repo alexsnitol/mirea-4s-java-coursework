c4f01a8af29755ef1a39f0177ac252cfc12b37ee
kozlovsasha2002 <kozlov_aleksanr24@mail.ru>
2022-05-22 22:17:48 +0300

    Fixed UserRepository.java

--- src/main/java/com/ineri/ineri_lk/repository/UserRepository.java
+++ src/main/java/com/ineri/ineri_lk/repository/UserRepository.java
@@ -4,11 +4,17 @@ import com.ineri.ineri_lk.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * @author Kozlov Alexander
  * @version 1.0
  */
 
 @Repository
-public interface UserRepository extends JpaRepository<Long, User> {
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByLogin(String username);
+    Boolean existsByLogin(String login);
+    Boolean existsByEmail(String email);
+    Boolean existsByPhoneNumber(String phoneNumber);
 }
