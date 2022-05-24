ea0a78869182f8283031851fd78c63dd1d8a0d8c
alexsnitol <sslotin74@gmail.com>
2022-05-24 15:17:25 +0300

    added MVCConfig class

---
+++ src/main/java/com/ineri/ineri_lk/configuration/MVCConfig.java
@@ -0,0 +1,19 @@
+package com.ineri.ineri_lk.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+@EnableWebMvc
+public class MVCConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/styles/**")
+                .addResourceLocations("classpath:/static/styles/");
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/");
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/");
+    }
+}
