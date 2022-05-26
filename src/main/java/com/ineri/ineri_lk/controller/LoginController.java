c8be02710dd2e51bfea28b74852bef39a69c4d76
kozlovsasha2002 <kozlov_aleksanr24@mail.ru>
2022-05-26 13:37:18 +0300

    LoginController.java fixed.

--- src/main/java/com/ineri/ineri_lk/controller/LoginController.java
+++ src/main/java/com/ineri/ineri_lk/controller/LoginController.java
@@ -1,7 +1,15 @@
 package com.ineri.ineri_lk.controller;
 
+
+import com.ineri.ineri_lk.model.User;
+import com.ineri.ineri_lk.service.impl.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * @author Kozlov Alexander
@@ -10,8 +18,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-    @GetMapping("login")
-    public String loginPage() {
-        return "";
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/login")
+    public String getLoginPage(User user, Model model) {
+        model.addAttribute("user", user);
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute("user") User user) {
+        User userDB = userService.getUserByUsername(user.getUsername());
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        if (userDB != null && encoder.encode(user.getPassword()).equals(userDB.getPassword()))
+            return "redirect:/" + user.getUsername() + "/catalog";
+        else
+            return "redirect:/login";
     }
 }
