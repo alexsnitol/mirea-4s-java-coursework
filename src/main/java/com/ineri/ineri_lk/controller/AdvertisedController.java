452a5a9c41149a8ab82db693647b5ab6be247832
alexsnitol <sslotin74@gmail.com>
2022-06-03 18:47:13 +0300

    advertised and other

--- src/main/java/com/ineri/ineri_lk/controller/AdvertisedController.java
+++ src/main/java/com/ineri/ineri_lk/controller/AdvertisedController.java
@@ -5,11 +5,10 @@ import com.ineri.ineri_lk.service.impl.*;
 import com.ineri.ineri_lk.util.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.HttpRequestHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
