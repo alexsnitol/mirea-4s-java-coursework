da43b3d281c67a7e5be4e55d6c6611a40f5cd91e
alexsnitol <sslotin74@gmail.com>
2022-06-05 15:12:50 +0300

    template for users form

--- src/main/java/com/ineri/ineri_lk/controller/SignupController.java
+++ src/main/java/com/ineri/ineri_lk/controller/SignupController.java
@@ -2,12 +2,15 @@ package com.ineri.ineri_lk.controller;
 
 import com.ineri.ineri_lk.model.User;
 import com.ineri.ineri_lk.service.impl.UserServiceImpl;
+import com.ineri.ineri_lk.util.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * @author Kozlov Alexander
@@ -26,15 +29,29 @@ public class SignupController {
     }
 
     @PostMapping("/signup")
-    public String saveUser(@ModelAttribute("user") User user, Model model) {
+    public String saveUser(@ModelAttribute("user") User user, Model model, @RequestParam("image") MultipartFile multipartFile) {
         User userDB = userServiceImpl.getUserByUsername(user.getUsername());
 
         if (!user.getPassword().equals(user.getConfirmPassword())) {
             model.addAttribute("passwordError", "Пароли не совпадают!");
             return "signup";
         } else {
+            uploadPhoto(user, multipartFile);
             userServiceImpl.saveUser(user);
             return "redirect:/login";
+        }
+    }
+
+    private void uploadPhoto(User user, MultipartFile multipartFile) {
+        if (!multipartFile.getResource().getFilename().equals("")) {
+            String fileName = user.getId() + ".jpg";
+            String uploadDir = "src/main/resources/upload/userdata";
+            try {
+                FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+                user.setPhotoPath("/upload/userdata/" + fileName);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- src/main/java/com/ineri/ineri_lk/controller/UserController.java
+++ src/main/java/com/ineri/ineri_lk/controller/UserController.java
@@ -5,13 +5,12 @@ import com.ineri.ineri_lk.model.Role;
 import com.ineri.ineri_lk.model.User;
 import com.ineri.ineri_lk.service.impl.FavoriteServiceImpl;
 import com.ineri.ineri_lk.service.impl.UserServiceImpl;
+import com.ineri.ineri_lk.util.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Collections;
@@ -36,24 +35,37 @@ public class UserController {
 
     @GetMapping("/profile")
     public ModelAndView showProfile(@PathVariable("username") String username) {
-        ModelAndView mv = new ModelAndView("test_view_profile");
+        ModelAndView mv = new ModelAndView("view_profile");
         mv = authController.setupUser(mv);
         return mv;
     }
 
     @GetMapping("/profile/edit")
     public ModelAndView updateUserForm(@PathVariable("username") String username) {
-        ModelAndView mv = new ModelAndView("test_edit_user_profile");
-        mv.addObject("username", username);
-        mv.addObject("user", userServiceImpl.getUserByUsername(username));
+        ModelAndView mv = new ModelAndView("edit_profile");
+        authController.setupUser(mv);
         return mv;
     }
 
     @PostMapping("/profile/edit")
-    public String updateUser(User user){
+    public String updateUser(User user, @RequestParam("image") MultipartFile multipartFile){
+        uploadPhoto(user, multipartFile);
         user.getRoles().add(Role.ROLE_USER);
         userServiceImpl.updateUser(user);
         return "redirect:/" + user.getUsername() + "/profile";
+    }
+
+    private void uploadPhoto(User user, MultipartFile multipartFile) {
+        if (!multipartFile.getResource().getFilename().equals("")) {
+            String fileName = user.getId() + ".jpg";
+            String uploadDir = "src/main/resources/upload/userdata";
+            try {
+                FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+                user.setPhotoPath("/upload/userdata/" + fileName);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @GetMapping("/favorites")
